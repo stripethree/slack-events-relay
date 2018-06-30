e401f61e70c2c78ebeccce482b403553c453c171
Jeff Israel <jeffrey_israel@condenast.com>
2018-06-29 17:26:43 -0700

    Start express with herolu compatible PORT env var

--- src/index.js
+++ src/index.js
@@ -6,5 +6,14 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }))
 app.get('/', (req, res) => res.send('🤖'));
 
+const port = process.env.PORT || 8080;
+app.listen(port, (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('Server running on port %s', port);
+});
+
+
 const slack = new Slack(process.env.SLACK_BOT_TOKEN);
 slack.start();
