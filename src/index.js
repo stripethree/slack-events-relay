ad23c94ebe29f09469c6aca21a924753e5e5ea9a
Jeff Israel <jeffrey_israel@condenast.com>
2018-06-29 17:19:27 -0700

    Add simple express server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+const express = require('express');
 const { Slack } = require('./slack');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }))
+app.get('/', (req, res) => res.send('🤖'));
+
 const slack = new Slack(process.env.SLACK_BOT_TOKEN);
 slack.start();
