55ecf5286fa7b04f996adea4ccba2488b3949c58
Jeff Israel <jeffrey_israel@condenast.com>
2018-07-01 13:46:51 -0700

    Stub for relay test

--- test/app.spec.js
+++ test/app.spec.js
@@ -4,12 +4,18 @@ const request = require('supertest');
 const { app, verifyRelayReq } = require('../src/index');
 
 describe('app', () => {
-  it('provides a default route', () => request(app)
-    .get('/')
-    .expect(200)
-    .then((res) => {
-      assert.equal(res.text, '🤖');
-    }));
+  describe('default route', () => {
+    it('provides a default route', () => request(app)
+      .get('/')
+      .expect(200)
+      .then((res) => {
+        assert.equal(res.text, '🤖');
+      }));
+  });
+
+  describe('relay route', () => {
+    xit('sends a Slack message with POST data');
+  });
 });
 
 describe('helpers', () => {
