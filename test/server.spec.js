3532cf5757de41d8df67fd5b9d7e39a8b4f5d028
Jeff Israel <jeffrey_israel@condenast.com>
2018-07-01 13:14:29 -0700

    Update to satisfy the linter

--- test/server.spec.js
+++ test/server.spec.js
@@ -4,12 +4,10 @@ const request = require('supertest');
 const { app } = require('../src/index');
 
 describe('app', () => {
-  it('provides a root route', () => {
-    return request(app)
-      .get('/')
-      .expect(200)
-      .then((res) => {
-        assert.equal(res.text, '🤖');
-      });
-  });
+  it('provides a root route', () => request(app)
+    .get('/')
+    .expect(200)
+    .then((res) => {
+      assert.equal(res.text, '🤖');
+    }));
 });
